1143bf04bddd3d846e94e04389bcc3c9ac05b626
DevRappers <devrappers@gmail.com>
2019-11-21 10:45:45 +0900

    First Hooks Query ( Client에서 만든 쿼리를 사용해보기 )

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { gql } from 'apollo-boost';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../Styles/GlobalStyles';
 import Theme from '../Styles/Theme';
 import AppRouter from './Router';
+import { useQuery } from 'react-apollo-hooks';
+
+// @client를 적지 않으면 apollo가 서버로 요청을 하게 됨
+// gql로 쿼리를 만듬
+const QUERY = gql`
+	{
+		isLoggedIn @client
+	}
+`;
 
 function App() {
+	const { data: { isLoggedIn } } = useQuery(QUERY);
+
 	return (
 		<ThemeProvider theme={Theme}>
 			<div>
 				<GlobalStyles />
-				<AppRouter isLoggedIn={false} />
+				<AppRouter isLoggedIn={isLoggedIn} />
 			</div>
 		</ThemeProvider>
 	);
