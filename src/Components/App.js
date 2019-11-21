c179a6aaf286f0b2972012641524c7c66969ee68
DevRappers <devrappers@gmail.com>
2019-11-21 09:37:26 +0900

    GlobalStyles and Theme 만들기

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from '../Styles/GlobalStyles';
+import Theme from '../Styles/Theme';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <ThemeProvider theme={Theme}>
+      <GlobalStyles/>
+    </ThemeProvider>
   );
 }
 
