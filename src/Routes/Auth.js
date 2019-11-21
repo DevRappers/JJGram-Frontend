15f64f766cbcb35e5217837c8aeed0fdbcae889a
DevRappers <devrappers@gmail.com>
2019-11-21 15:11:59 +0900

    Auth Form with Hooks ( useState를 활용해 input상태 관리하기 )

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import Input from '../Components/Input';
 import Button from '../Components/Button';
+import useInput from '../Hooks/useInput';
 
 const Wrapper = styled.div`
 	min-height: 80vh;
@@ -48,29 +49,34 @@ const Form = styled(Box)`
 
 function Auth() {
 	const [ action, setAction ] = useState('logIn');
+	const username = useInput('');
+	const password = useInput('');
+	const firstName = useInput('');
+	const lastName = useInput('');
+	const email = useInput('');
 
 	return (
 		<Wrapper>
 			<Form>
 				{action === 'logIn' ? (
 					<form>
-						<Input placeholder={'사용자 이름'} />
-						<Input placeholder={'비밀번호'} />
+						<Input placeholder={'사용자 이름'} {...username} />
+						<Input placeholder={'비밀번호'} {...password} type="password" />
 						<Button text={'로그인'} />
 					</form>
 				) : (
 					<form>
-						<Input placeholder={'성'} />
-						<Input placeholder={'이름'} />
-						<Input placeholder={'이메일 주소'} />
-						<Input placeholder={'사용자 이름'} />
-						<Input placeholder={'비밀번호'} />
+						<Input placeholder={'성'} {...firstName} />
+						<Input placeholder={'이름'} {...lastName} />
+						<Input placeholder={'이메일 주소'} {...email} type="email" />
+						<Input placeholder={'사용자 이름'} {...username} />
+						<Input placeholder={'비밀번호'} {...password} type="password" />
 						<Button text={'가입'} />
 					</form>
 				)}
 			</Form>
 			<StateChanger>
-				{action == 'logIn' ? (
+				{action === 'logIn' ? (
 					<div>
 						계정이 없으신가요? <Link onClick={() => setAction('signUp')}>가입하기</Link>
 					</div>
