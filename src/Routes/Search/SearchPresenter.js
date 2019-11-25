5c95c84c1cc3ea66728438c54a5c8a2587e84e18
DevRappers <devrappers@gmail.com>
2019-11-25 10:48:14 +0900

    Search Screen UI ( 검색 화면 만들기 검색했을때 user는 UserCard Component로 보여지게 하기 )

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -2,15 +2,62 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import FatText from '../../Components/FatText';
+import Loader from '../../Components/Loader';
+import UserCard from '../../Components/userCard';
 
-const Wrapper = styled.div`
-	height: 50vh;
-	text-align: center;
+const Wrapper = styled.div`height: 50vh;`;
+
+const Section = styled.div`
+	margin-bottom: 50px;
+	display: grid;
+	grid-gap: 25px;
+	grid-template-columns: repeat(4, 1fr);
+	grid-template-rows: 160px;
+	grid-auto-rows: 160px;
 `;
 
-const SearchPresenter = ({ searchTerm, loading }) => (
-	<Wrapper>{searchTerm === undefined && <FatText text={'Search for something'} />}</Wrapper>
-);
+const SearchPresenter = ({ searchTerm, loading, data }) => {
+	if (searchTerm === undefined) {
+		return (
+			<Wrapper>
+				<FatText text="Search for something" />
+			</Wrapper>
+		);
+	} else if (loading === true) {
+		return (
+			<Wrapper>
+				<Loader />
+			</Wrapper>
+		);
+	} else if (data && data.searchUser && data.searchPost) {
+		return (
+			<Wrapper>
+				<Section>
+					{data.searchUser.length === 0 ? (
+						<FatText text="유저를 찾을 수 없습니다." />
+					) : (
+						data.searchUser.map((user) => (
+							<UserCard
+								key={user.id}
+								username={user.name}
+								isFollowing={user.isFollowing}
+								url={user.avatar}
+								isSelf={user.isSelf}
+							/>
+						))
+					)}
+				</Section>
+				<Section>
+					{data.searchPost.length === 0 ? (
+						<FatText text="해당하는 포스트가 없습니다." />
+					) : (
+						data.searchPost.map((post) => null)
+					)}
+				</Section>
+			</Wrapper>
+		);
+	}
+};
 
 SearchPresenter.propTypes = {
 	searchTerm: PropTypes.string,
